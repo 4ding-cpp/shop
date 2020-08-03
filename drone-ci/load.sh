7f7044d4a6cee538decaee56005e5b5f521067a1
4ding-cp <4dingsite@gmail.com>
2020-08-03 23:18:47 +0800

    o

--- drone-ci/load.sh
+++ drone-ci/load.sh
@@ -2,19 +2,22 @@
 
 
 prepare() {
-    echo ">>>$NORMAL_SERVICE_PORT_WEB1" 
-
     if [ "$NORMAL_SERVICE_PORT_WEB1" = "" ]; then
-        return true
+        return "env NORMAL_SERVICE_PORT_WEB1 not set"
     fi
-
     sed -i "s/NGINX_PORT/"$NORMAL_SERVICE_PORT_WEB1"/g" /etc/nginx/conf.d/default.conf
 
-    echo ">>>$NORMAL_SERVICE_PORT_WEB1" 
+    if [ "$NORMAL_SERVICE_HOST" = "" ]; then
+        return "env NORMAL_SERVICE_HOST not set"
+    fi
+    sed -i "s/GRPC_HOST/"$NORMAL_SERVICE_HOST"/g" /etc/nginx/conf.d/default.conf
 
+    if [ "$NORMAL_SERVICE_PORT_SHOP" = "" ]; then
+        return "env NORMAL_SERVICE_PORT_SHOP not set"
+    fi
+    sed -i "s/GRPC_PORT/"$NORMAL_SERVICE_PORT_SHOP"/g" /etc/nginx/conf.d/default.conf
+    
     nginx -g "daemon off;"
 }
 
 prepare
-
-echo "env NORMAL_SERVICE_PORT_WEB1 not set"
