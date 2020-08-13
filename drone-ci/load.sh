950852b3684a64b5b49b593fa4ecac0126dfb915
4ding-cp <4dingsite@gmail.com>
2020-08-14 01:07:36 +0800

    auto

--- drone-ci/load.sh
+++ drone-ci/load.sh
@@ -5,16 +5,15 @@ prepare() {
     if [ "$WEB1_SERVICE_PORT" = "" ]; then
         return "env WEB1_SERVICE_PORT not set"
     fi
-    sed -i "s/NGINX_PORT/"$WEB1_SERVICE_PORT"/g" /etc/nginx/conf.d/default.conf
-
     if [ "$SHOP_SERVICE_HOST" = "" ]; then
         return "env SHOP_SERVICE_HOST not set"
     fi
-    sed -i "s/GRPC_HOST/"$SHOP_SERVICE_HOST"/g" /etc/nginx/conf.d/default.conf
-
     if [ "$SHOP_SERVICE_PORT" = "" ]; then
         return "env SHOP_SERVICE_PORT not set"
     fi
+
+    sed -i "s/NGINX_PORT/"$WEB1_SERVICE_PORT"/g" /etc/nginx/conf.d/default.conf
+    sed -i "s/GRPC_HOST/"$SHOP_SERVICE_HOST"/g" /etc/nginx/conf.d/default.conf
     sed -i "s/GRPC_PORT/"$SHOP_SERVICE_PORT"/g" /etc/nginx/conf.d/default.conf
     
     nginx -g "daemon off;"
