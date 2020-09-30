e663c92da41db4aae9b164e7267b130cfeb6282e
4ding-cp <4dingsite@gmail.com>
2020-09-30 23:07:00 +0800

    b

--- drone-ci/load.sh
+++ drone-ci/load.sh
@@ -8,17 +8,17 @@ prepare() {
     if [ "$SHOP_SERVICE_PORT" = "" ]; then
         return "env SHOP_SERVICE_PORT not set"
     fi
-    if [ "$ENTRYPOINT_SERVICE_HOST" = "" ]; then
-        return "env ENTRYPOINT_SERVICE_HOST not set"
+    if [ "$HTTP_SERVICE_HOST" = "" ]; then
+        return "env HTTP_SERVICE_HOST not set"
     fi
-    if [ "$ENTRYPOINT_SERVICE_PORT" = "" ]; then
-        return "env ENTRYPOINT_SERVICE_PORT not set"
+    if [ "$HTTP_SERVICE_PORT" = "" ]; then
+        return "env HTTP_SERVICE_PORT not set"
     fi
     
     sed -i "s/GRPC_HOST/"$SHOP_SERVICE_HOST"/g" /etc/nginx/conf.d/default.conf
     sed -i "s/GRPC_PORT/"$SHOP_SERVICE_PORT"/g" /etc/nginx/conf.d/default.conf
-    sed -i "s/HTTP_HOST/"$ENTRYPOINT_SERVICE_HOST"/g" /etc/nginx/conf.d/default.conf
-    sed -i "s/HTTP_PORT/"$ENTRYPOINT_SERVICE_PORT"/g" /etc/nginx/conf.d/default.conf
+    sed -i "s/HTTP_HOST/"$HTTP_SERVICE_HOST"/g" /etc/nginx/conf.d/default.conf
+    sed -i "s/HTTP_PORT/"$HTTP_SERVICE_PORT"/g" /etc/nginx/conf.d/default.conf
     
     nginx -g "daemon off;"
 }
